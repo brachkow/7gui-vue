fa6520d012cbbd7e2c2badb03f208ebb75c7afb2
Timur Brachkow <brachkow@fastmail.com>
2020-12-27 13:38:10 +0300

    Converter added

---
+++ src/components/pages/Converter/Converter.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+    <label class="flex justify-between leading-loose mb-4"
+      >Celsius:
+      <input
+        type="number"
+        class="bg-gray-200 leading-loose px-4 ml-4 rounded focus:outline-none text-right"
+        v-model="celsius"
+        placeholder="0"
+    /></label>
+    <label class="flex justify-between leading-loose"
+      >Fahrenheit:
+      <input
+        type="number"
+        class="bg-gray-200 leading-loose px-4 ml-4 rounded focus:outline-none text-right"
+        v-model="fahrenheit"
+        placeholder="0"
+    /></label>
+  </main>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        dataCelsius: 0,
+        dataFahrenheit: 0,
+      };
+    },
+    computed: {
+      celsius: {
+        get: function () {
+          return Math.round(Number(this.dataCelsius));
+        },
+        set: function (value) {
+          this.dataCelsius = value;
+          this.dataFahrenheit = value * (9 / 5) + 32;
+        },
+      },
+      fahrenheit: {
+        get: function () {
+          return Math.round(Number(this.dataFahrenheit));
+        },
+        set: function (value) {
+          this.dataCelsius = (value - 32) * (5 / 9);
+          this.dataFahrenheit = value;
+        },
+      },
+    },
+  };
+</script>
--- src/router/router.js
+++ src/router/router.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Counter from '@components/pages/Counter/Counter.vue';
+import Converter from '@components/pages/Converter/Converter.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/counter', component: Counter },
+  { path: '/converter', component: Converter },
 ];
 
 const router = new VueRouter({
