9b6d70c4d8d5f9c3b5bff789d7273129d4c62b2a
Timur Brachkow <brachkow@fastmail.com>
2020-12-27 14:52:14 +0300

    Timer added

---
+++ src/components/pages/Timer/Timer.vue
@@ -0,0 +1,47 @@
+<template>
+  <main style="min-width: 300px">
+    <label class="block mb-2 bg-gray-200 p-4 rounded">
+      Elapsed Time ({{ elapsed }}s):
+      <progress
+        :value="elapsed"
+        :max="duration"
+        class="block w-full mt-1"
+      ></progress>
+    </label>
+    <label class="block mb-2 bg-gray-200 p-4 rounded"
+      >Duration ({{ duration }}s):
+      <input
+        type="range"
+        min="0"
+        max="60"
+        step="1"
+        v-model="duration"
+        class="w-full block mt-1"
+      />
+    </label>
+    <button
+      class="bg-blue-600 text-white leading-loose px-4 rounded w-full focus:outline-none transition-colors"
+      @click="elapsed = 0"
+    >
+      Reset
+    </button>
+  </main>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        elapsed: 0,
+        duration: 100,
+      };
+    },
+    mounted() {
+      setInterval(() => {
+        if (this.elapsed < this.duration) {
+          this.elapsed = this.elapsed + 1;
+        }
+      }, 100);
+    },
+  };
+</script>
--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import Counter from '@components/pages/Counter/Counter.vue';
 import Converter from '@components/pages/Converter/Converter.vue';
 import Booker from '@components/pages/Booker/Booker.vue';
+import Timer from '@components/pages/Timer/Timer.vue';
 
 Vue.use(VueRouter);
 
@@ -11,6 +12,7 @@ const routes = [
   { name: 'Counter', path: '/counter', component: Counter },
   { name: 'Converter', path: '/converter', component: Converter },
   { name: 'Booker', path: '/booker', component: Booker },
+  { name: 'Timer', path: '/Timer', component: Timer },
 ];
 
 const router = new VueRouter({
